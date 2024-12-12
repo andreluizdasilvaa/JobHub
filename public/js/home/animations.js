e94d62d1df26bc09f175bd0ab459e72f5fb3a0ae
André Luiz <154485939+andreluizdasilvaa@users.noreply.github.com>
2024-12-12 19:56:07 -0300

    LP - Home 1.0.1

--- public/js/home/animations.js
+++ public/js/home/animations.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 if (count < target) {
                     counter.innerText = Math.ceil(count + increment);
-                    setTimeout(updateCount, 10);
+                    setTimeout(updateCount, 15);
                 } else {
                     counter.innerText = target;
                 }
@@ -33,4 +33,20 @@ document.addEventListener('DOMContentLoaded', () => {
             animateCounters();
         }
     });
-})
+
+    //Sumir o header após tanto de rolagem
+    const header = document.querySelector('.btn_header');
+
+    // Adiciona o evento de rolagem
+    window.addEventListener('scroll', () => {
+        const scrollPosition = window.scrollY;
+        const viewportHeight = window.innerHeight;
+
+        // Adiciona ou remove a classe 'scrolled' com base na posição do scroll
+        if (scrollPosition > 0.8 * viewportHeight) { 
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+});
