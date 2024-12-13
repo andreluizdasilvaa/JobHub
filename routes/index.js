7edb03585f496a6b92770d125a761baa08abb6ec
André Luiz <154485939+andreluizdasilvaa@users.noreply.github.com>
2024-12-12 23:40:42 -0300

    Termos de Uso e Politicas de Privacidade Prontas
    + Inicio da pagina de cadastro iniciado

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../public/html/home.html'));
 });
+
+router.get('/terms-of-use-and-privacy-policies', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/html/TermsAndPlpriv.html'));
+});
+
+router.get('/cadastro', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/html/'))
+})
 
 module.exports = router;
